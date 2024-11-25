c213a91cc20080eeaa0115a2d25169f2f3016257
joseph skokan <joeskokan20@gmail.com>
2024-11-24 23:52:40 -0500

    category model

---
+++ church-cms-backend/models/categories.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Category = sequelize.define("categories", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+module.exports = Category;
